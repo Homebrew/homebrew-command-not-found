97786567ad0135a52e45c4cf3ff193aa70e4cd5b
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-25 15:12:30 +0100

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# brew-command-not-found
+
+This project try to reproduce Ubuntu’s `command-not-found` for homebrew users
+on OSX.
+
+On a Ubuntu, when you try to use a command that doesn’t exist locally but is
+available through a package, Bash will suggest you a command to install this
+package. Using this script, you can replicate this behavior on OSX:
+
+```
+# on Ubuntu
+$ when
+The program 'when' is currently not installed.  You can install it by typing:
+sudo apt-get install when
+
+# on OSX with Homebrew
+$ when
+The program 'when' is currently not installed. You can install it by typing:
+  brew install when
+```
+
+## Install
+
+Download `handle.sh` and source it somewhere in your `.bashrc`:
+
+```sh
+. /path/to/handle.sh
+```
+
+## Contributing
+
+Feel free to make a pull-request if you want to add MacPort or Fink support!
---
+++ handle.sh
@@ -0,0 +1,40 @@
+# 
+# brew-command-not-found script for OS X
+#
+# Usage: Source it somewhere in your .bashrc
+#
+# Author: Baptiste Fontaine
+# License: MIT
+# Version: 0.1.0
+# 
+
+[ ! -z "$(which brew)" ] && command_not_found_handle() {
+
+    # <from Linux Journal>
+    #   http://www.linuxjournal.com/content/bash-command-not-found
+
+    export TEXTDOMAIN=command-not-found
+
+    # do not run when inside Midnight Commander or within a Pipe
+    if test -n "$MC_SID" -o ! -t 1 ; then
+        echo $"$1: command not found"
+        return 127
+    fi
+
+    # </from Linux Journal>
+
+    local path="$(brew --prefix)/Library/Formula"
+    local f=$(\grep -lI -e "bin\.install..*$1" $path/*.rb 2>/dev/null)
+
+    if [ -z "$f" ]; then
+        echo $"$1: command not found"
+        return 127
+    fi
+
+    f=${f##*/}
+    f=${f%%.rb}
+
+    echo $"The program '$1' is currently not installed. You can install it by typing:"
+    echo $"  brew install $f"
+
+}
