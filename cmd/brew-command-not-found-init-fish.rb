a9933e0dab11b1d3d981fb389c5159e050bf3397
lordlycastle <lordlycastle@hotmail.com>
2015-08-11 00:10:41 +0100

    Same brew command for fish. Improved handling of custom functions dir. Formatted Readme along other feedback from @bfontaine

--- cmd/brew-command-not-found-init-fish.rb
+++
@@ -1,7 +0,0 @@
-
-require "fileutils"
-
-file = File.expand_path("#{File.dirname(__FILE__)}/../handler.fish")
-destination = ARGV[0] ? ARGV[0]+"command-not-found.fish" : File.expand_path("~/.config/fish/functions/command-not-found.fish")
-
-FileUtils.cp(file, destination)
--- cmd/brew-command-not-found-init.rb
+++ cmd/brew-command-not-found-init.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
+require "fileutils"
 
-puts File.read(File.expand_path "#{File.dirname(__FILE__)}/../handler.sh")
+if (!ARGV.empty? and ARGV[0] == "--fish")
+	file = File.expand_path("#{File.dirname(__FILE__)}/../handler.fish")
+	destination = (ARGV[1] != nil) ? File.expand_path(File.join(ARGV[1], "command-not-found.fish")) : File.expand_path("~/.config/fish/functions/command-not-found.fish")
+
+	FileUtils.cp(file, destination)
+else
+	puts File.read(File.expand_path "#{File.dirname(__FILE__)}/../handler.sh")
+end
