a49001349deec423dbdde00bf88c5ced6b8b6cbd
Rylan Polster <rslpolster@gmail.com>
2022-07-25 18:29:37 +0200

    Handler: ignore `-h`, `--help`, `--usage`, and `-?` (#111)

--- handler.fish
+++ handler.fish
@@ -1,6 +1,10 @@
 function fish_command_not_found
     set -l cmd $argv[1]
-    set -l txt (brew which-formula --explain $cmd 2> /dev/null)
+    set -l txt
+
+    if not contains -- "$cmd" "-h" "--help" "--usage" "-?"
+        set txt (brew which-formula --explain $cmd 2> /dev/null)
+    end
 
     if test -z "$txt"
         __fish_default_command_not_found_handler $cmd
